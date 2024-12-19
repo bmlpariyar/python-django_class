f9a37fca730d0239e24ffd0726b991a3f8872d71
bimal pariyar <bimalpariyar697@gmail.com>
2024-12-19 18:11:59 +0545

    feat:crud operation of the book model

--- book_review_platform/book_review/urls.py
+++ book_review_platform/book_review/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from .views import index, about, contact, book_details
+from .views import index, about, contact, book_details, new_boook, delete_book, edit_book
 
 urlpatterns = [
     path('', index, name="home"),
     path('about/', about, name="about"),
     path('contact/', contact, name="contact"),
-    path('book_details/<int:book_id>/', book_details, name="book_details" )
+    path('book_details/<int:book_id>/', book_details, name="book_details" ),
+    path('new_book/', new_boook, name="new_book"),
+    path('delete_book/<int:book_id>/', delete_book, name="delete_book"),
+    path('edit_book/<int:book_id>/', edit_book, name="edit_book")
+    
 ]
--- book_review_platform/book_review/views.py
+++ book_review_platform/book_review/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
+from .forms import BookForm
 # Create your views here.
 
 def index(request):
@@ -20,3 +21,35 @@ def book_details(request, book_id):
     books = Book.objects.get(id=book_id)
     context = {'book': books}
     return render(request, 'book_details.html', context)
+
+
+def new_boook(request):
+    if request.method == "POST":
+        form= BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = BookForm()
+    context = {'form': form}
+    return render(request, 'new_book.html', context)
+
+
+
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete()
+    return redirect("home")
+
+
+def edit_book(request, book_id):
+    book=Book.objects.get(id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookForm(instance=book)
+    context = {'form': form}
+    return render(request, 'new_book.html', context)
+    
